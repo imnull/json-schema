046e4ad5f7a5cab9438317035c85199c9618f6e4
marvin <imnull@outlook.com>
2021-08-03 15:49:59 +0800

    json-schema-traverse

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,26 @@
 import { multiCheck } from './tools'
+import traverse from 'json-schema-traverse'
+
+const schema = {
+    properties: {
+        foo: { type: 'string' },
+        // bar: { type: 'integer' },
+        'a/b': {
+            type: 'object',
+            properties: {
+                a: { type: 'integer' },
+                'a/b/c': {
+                    type: 'string'
+                }
+            }
+        }
+    }
+}
+
+traverse(schema, {
+    cb: (schema, path, ...args) => {
+        console.log(path)
+    }
+})
 
 export default multiCheck
